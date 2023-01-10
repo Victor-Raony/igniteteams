17bce0928181216061ba0ae54ed10d982c5253bd
Victor <victor_raony12@hotmail.com>
2023-01-10 06:09:46 -0300

    Organizando Project

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react-native'
+
+import { Groups } from './src/screens/Groups';
+
+export default function App() {
+  return (
+    <Groups />
+  );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react-native'
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export function Groups() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Groups</Text>
     </View>
   );
 }
@@ -13,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff', 
     alignItems: 'center',
     justifyContent: 'center',
   },
